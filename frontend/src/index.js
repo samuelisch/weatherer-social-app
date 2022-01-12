102b6d26abec455330f23eadd26d770298c1b75d
Samuelisch <samuelchanwk@hotmail.com>
2022-01-12 16:27:59 +0800

    added json server as dev dependency to display mock details before implementing backend or application

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,14 +1,10 @@
-import React from 'react';
-import { ReactDOM } from 'react';
-import App from './App';
-import { store } from './app/store';
-import { Provider } from 'react-redux';
+import React from 'react'
+import ReactDOM from 'react-dom';
+import App from './App'
 
 ReactDOM.render(
   <React.StrictMode>
-    <Provider store={store}>
       <App />
-    </Provider>
   </React.StrictMode>,
   document.getElementById('root')
 );
