7af0d58db3bb80c5b7ea886fb9ed3e819332d34a
Samuelisch <samuelchanwk@hotmail.com>
2022-01-14 11:25:34 +0800

    updated base url of post services for axios

--- frontend/src/services/post.js
+++ frontend/src/services/post.js
@@ -1,6 +1,6 @@
 import axios from 'axios'
 
-const url = 'http://localhost:3001/posts'
+const url = 'http://localhost:3001/api/posts'
 
 const getAll = async () => {
   const response = await axios.get(url)
