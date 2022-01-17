aac3ab2e6938b336a88e80c2c266f964930c6fa6
Samuelisch <samuelchanwk@hotmail.com>
2022-01-17 14:42:21 +0800

    bundled reducers together in store file, added temporary code to App component to display login UI and feedback

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,25 @@
-import React from 'react'
-import PostForm from './components/posts/PostForm';
-import PostsList from './components/posts/PostsList'
+import React, { useEffect } from 'react'
+import { useDispatch, useSelector } from 'react-redux';
+import { initializeUser } from './reducers/loginReducer';
+import LoginForm from './components/home/login/LoginForm';
+import PostForm from './components/main/posts/PostForm';
+import PostsList from './components/main/posts/PostsList'
 
 const App = () => {
+  const dispatch = useDispatch()
+  const user = useSelector(state => state.login)
+
+  useEffect(() => {
+    dispatch(initializeUser())
+  }, [dispatch])
+
   return (
     <div className="App">
       <h1>Weatherer</h1>
+      {user &&
+        <h3>Welcome {user.username}</h3>
+      }
+      <LoginForm />
       <PostForm />
       <PostsList />
     </div>
--- frontend/src/store.js
+++ frontend/src/store.js
@@ -2,9 +2,11 @@ import { createStore, combineReducers, applyMiddleware } from '@reduxjs/toolkit'
 import thunk from 'redux-thunk'
 import { composeWithDevTools } from 'redux-devtools-extension'
 import postReducer from './reducers/postReducer'
+import loginReducer from './reducers/loginReducer'
 
 const reducer = combineReducers({
-  posts: postReducer
+  posts: postReducer,
+  login: loginReducer
 })
 
 const store = createStore(
