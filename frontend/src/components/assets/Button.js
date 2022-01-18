229274a06bc11f666f625e02f300d9ef02248484
Samuelisch <samuelchanwk@hotmail.com>
2022-01-18 15:19:48 +0800

    added parameter for className

--- frontend/src/components/assets/Button.js
+++ frontend/src/components/assets/Button.js
@@ -1,8 +1,8 @@
 import React from 'react'
 
-const Button = ({ type="button" ,text, handleClick }) => {
+const Button = ({ className, type="button" ,text, handleClick }) => {
   return (
-    <button type={type} onClick={handleClick}>{text}</button>
+    <button className={className} type={type} onClick={handleClick}>{text}</button>
   )
 }
 
