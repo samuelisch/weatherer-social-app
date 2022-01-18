6c683b80062dd617547fdf5aa1a9eeafe45634f1
Samuelisch <samuelchanwk@hotmail.com>
2022-01-18 09:35:02 +0800

    added likedPosts to user schema, to track posts that user as liked

--- backend/models/user.js
+++ backend/models/user.js
@@ -22,6 +22,12 @@ const userSchema = new mongoose.Schema({
       type: mongoose.Schema.Types.ObjectId,
       ref: 'Post'
     }
+  ],
+  likedPosts: [
+    {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'Post'
+    }
   ]
 });
 
