765fc9a521e2fc22feaa0b1f7ab8f7ebd146fbaa
Samuelisch <samuelchanwk@hotmail.com>
2022-01-19 17:42:01 +0800

    added global style

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,24 @@
 import React from 'react'
 import Routers from './routers/Routers'
+import { createGlobalStyle } from 'styled-components'
+
+const GlobalStyle = createGlobalStyle`
+  :root,
+  body {
+    background: rgb(245, 245, 245);
+    font-family: helvetica;
+    font-size: 10px;
+    margin: 0;
+  }
+`
 
 const App = () => {
   return (
     // Notification component
-    <Routers />
+    <div className="content">
+      <GlobalStyle />
+      <Routers />
+    </div>
   )
 
 }
