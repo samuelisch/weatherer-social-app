229f38d94af042ec6146c79f7e4e3e0f788a2966
Samuelisch <samuelchanwk@hotmail.com>
2022-01-19 17:41:41 +0800

    added a Login component that imports containers, which encapsulates LoginForm component

---
+++ frontend/src/components/home/login/Login.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import LoginForm from './LoginForm'
+import Container from '../Container'
+
+const Login = () => {
+  return (
+    <Container>
+      <LoginForm />
+    </Container>
+  )
+}
+
+export default Login
--- frontend/src/components/home/login/LoginForm.js
+++ frontend/src/components/home/login/LoginForm.js
@@ -4,6 +4,65 @@ import Button from '../../assets/Button'
 import { useDispatch } from 'react-redux'
 import { loginUser } from '../../../reducers/loginReducer'
 import { useNavigate } from 'react-router-dom'
+import styled from 'styled-components'
+
+const StyledContainer = styled.div`
+  width: 200px;
+`
+
+const StyledHeader = styled.h2`
+  font-size: 2rem;
+  color: rgb(245, 245, 245);
+`
+
+const StyledForm = styled.form`
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  margin: 40px 0 40px;
+  min-height: 120px;
+
+  .loginInput {
+    width: 95%;
+    background-color: rgb(15, 15, 15);
+    color: rgb(245, 245, 245);
+    margin-bottom: 10px;
+    border-radius: 4px;
+    border: 1px solid rgb(110, 110, 110);
+    padding: 8px 3px;
+  }
+
+  .loginButton {
+    margin-top: 20px;
+    padding: 5px;
+    border-radius: 15px;
+    background-color: rgb(240, 240, 240);
+    color: rgb(10, 10, 10);
+    font-weight: bold;
+  }
+
+  .loginButton:hover {
+    background-color: rgb(220, 220, 220);
+    cursor: pointer;
+  }
+`
+
+const StyledText = styled.p`
+  font-size: 1rem;
+  color: rgb(110, 110, 110);
+  display: flex;
+  align-items: center;
+  
+  span {
+    margin-left: 5px;
+    color: rgb(120, 210, 250)
+  }
+
+  span:hover {
+    text-decoration: underline;
+    cursor: pointer;
+  }
+`
 
 const LoginForm = () => {
   const navigate = useNavigate()
@@ -16,7 +75,7 @@ const LoginForm = () => {
 
     try {
       await dispatch(loginUser({username, password}))
-      navigate('/')
+      navigate('/main')
     } catch (error) {
       console.log(error)
     }
@@ -25,28 +84,33 @@ const LoginForm = () => {
   }
 
   return (
-      <>
-        <h2>Login</h2>
-        <form onSubmit={loginHandler}>
+      <StyledContainer>
+        <StyledHeader>Log in to Weatherer</StyledHeader>
+        <StyledForm onSubmit={loginHandler}>
           <div>
-            <Input
-              name='loginUsername'
-              id='loginUsername'
-              type='text'
-              placeholder='Username'
-            />
+            <div>
+              <Input
+                className="loginInput"
+                name='loginUsername'
+                id='loginUsername'
+                type='text'
+                placeholder='Username'
+              />
+            </div>
+            <div>
+              <Input
+                className="loginInput"
+                name='loginPassword'
+                id='loginPassword'
+                type='password'
+                placeholder='Password'
+              />
+            </div>
           </div>
-          <div>
-            <Input 
-              name='loginPassword'
-              id='loginPassword'
-              type='password'
-              placeholder='Password'
-            />
-          </div>
-          <Button type='submit' text='login' />
-        </form>
-      </>
+          <Button className="loginButton" type='submit' text='Login' />
+        </StyledForm>
+        <StyledText>Don't have an account?<span onClick={() => navigate('/signup')}>Sign up</span></StyledText>
+      </StyledContainer>
   )
 }
 
