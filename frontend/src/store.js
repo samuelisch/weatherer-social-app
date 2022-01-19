ac4b96fb6e22dd7844d3a5ce09a2743ee1e00572
Samuelisch <samuelchanwk@hotmail.com>
2022-01-19 21:15:54 +0800

    bundled userReducer with other reducers

--- frontend/src/store.js
+++ frontend/src/store.js
@@ -3,10 +3,12 @@ import thunk from 'redux-thunk'
 import { composeWithDevTools } from 'redux-devtools-extension'
 import postReducer from './reducers/postReducer'
 import loginReducer from './reducers/loginReducer'
+import userReducer from './reducers/userReducer'
 
 const reducer = combineReducers({
   posts: postReducer,
-  login: loginReducer
+  login: loginReducer,
+  users: userReducer
 })
 
 const store = createStore(
