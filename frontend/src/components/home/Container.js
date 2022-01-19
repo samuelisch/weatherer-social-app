2d6001af7cc8bd9d9543b6420ee3b5266beb9943
Samuelisch <samuelchanwk@hotmail.com>
2022-01-19 17:40:37 +0800

    minor styling changes for Container components, home component navigates to login form, and signup form is shown as popup instead

--- frontend/src/components/home/Container.js
+++ frontend/src/components/home/Container.js
@@ -1,29 +1,59 @@
 import React from 'react'
 import styled from 'styled-components'
+import { useNavigate } from 'react-router-dom'
 
 const OuterContainer = styled.div`
   position: fixed;
   width: 100%;
   height: 100%;
-  background-color: rgba(91, 112, 131, 0.4);
+  background-color: rgba(91, 112, 131, 0.9);
   display: flex;
   flex-basis: auto;
   justify-content: center;
   align-items: center;
+  top: 0;
 
 `
 
 const InnerContainer = styled.div`
-  position: relative;
-  dispaly: flex;
+  display: flex;
+  width: 80%;
+  max-width: 400px;
+  padding: 30px 0;
   flex-direction: column;
   align-items: center;
+  background-color: rgb(15, 15, 15);
+  border-radius: 10px;
+  position: relative;
+
+  .cancel {
+    position: absolute;
+    top: 8px; left: 8px;
+    width: 25px;
+    height: 25px;
+    border-radius: 50%;
+  }
+
+  .cancel:hover {
+    background: rgb(50, 50, 50);
+    cursor: pointer;
+  }
+
+  .cancel:after {
+    position: relative;
+    color: white;
+    content: "\\00d7";
+    font-size: 20px;
+    margin-left: 6px;
+  }
 `
 
 const Container = (props) => {
+  const navigate = useNavigate()
   return (
     <OuterContainer>
       <InnerContainer>
+        <div className="cancel" onClick={() => navigate('/')}></div>
         {props.children}
       </InnerContainer>
     </OuterContainer>
--- frontend/src/components/home/Home.js
+++ frontend/src/components/home/Home.js
@@ -1,23 +1,47 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import LoginForm from './login/LoginForm'
 import Button from '../assets/Button'
 import Container from './Container'
+import styled from 'styled-components'
+import { useNavigate } from 'react-router-dom'
+
+const StyledHome = styled.div`
+  padding-left: 10px;
+  position: relative;
+`
 
 const Home = () => {
-  const [loggingIn, setLoggingIn] = useState(false)
+  const navigate = useNavigate()
+  const [signUp, setSignUp] = useState(false)
+  const authenticated = window.localStorage.getItem('loggedAppUser')
+
+  useEffect(() => {
+    if (authenticated) {
+      navigate('/main')
+    }
+  }, [authenticated, navigate])
+
+  const signupUser = () => {
+    setSignUp(true)
+  }
 
   return (
     <div>
-      <h1>Join Weatherer today.</h1>
-      <div>
+      <StyledHome>
+        <h1>Join Weatherer today.</h1>
+        <Button
+          type="button"
+          text="Sign up"
+          handleClick={signupUser}
+        />
         <h3>Have an exisitng account?</h3>
           <Button 
             type="button"
             text="Log in"
-            handleClick={() => setLoggingIn(!loggingIn)}
+            handleClick={() => navigate('/login')}
           />
-      </div>
-      {loggingIn &&
+      </StyledHome>
+      {signUp &&
         <Container>
           <LoginForm />
         </Container>
