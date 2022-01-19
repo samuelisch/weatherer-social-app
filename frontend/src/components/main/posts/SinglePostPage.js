36aa5a1f6d4fc5134049108102fb3618d1e377b6
Samuelisch <samuelchanwk@hotmail.com>
2022-01-19 22:29:39 +0800

    added component to display details of single post on fresh page

--- frontend/src/components/main/posts/SinglePostPage.js
+++ frontend/src/components/main/posts/SinglePostPage.js
@@ -2,15 +2,18 @@ import React from 'react'
 import styled from 'styled-components'
 import { useParams } from 'react-router-dom'
 import { useSelector } from 'react-redux'
+import { getPostFromId } from '../../../reducers/postReducer'
 
 const SinglePostPage = () => {
   const params = useParams()
+  const { username, postId } = params
+  const post = useSelector(state => getPostFromId(state.posts, postId))
 
   return (
     <div>
-      <p>Content: </p>
-      <p>Likes: </p>
-      <p>by: </p>
+      <p>Content: {post.content}</p>
+      <p>Likes: {post.likes}</p>
+      <p>by: {username}</p>
     </div>
   )
 
