3283ca0707713eb8e23d78ad8213af7b5c40c81f
Samuelisch <samuelchanwk@hotmail.com>
2022-01-19 22:26:57 +0800

    added styling, navigation links to single post page and user page

--- frontend/src/components/main/posts/Post.js
+++ frontend/src/components/main/posts/Post.js
@@ -1,23 +1,41 @@
 import React, { useState } from 'react'
 import Button from '../../assets/Button'
 import styled from 'styled-components'
-import { Link } from 'react-router-dom'
+import { useNavigate, Link } from 'react-router-dom'
 
 const StyledItem = styled.li`
   border: 1px solid rgb(85, 85, 85);
   padding: 10px;
 
+  &:hover {
+    cursor: pointer;
+  }
+
   .postLikeButton {
     background-color: ${props => props.liked ? 'rgb(173, 226, 230)' : 'rgb(215, 215, 215)'};
+  }
+
+  .postLikeButton:hover {
+    background-color: rgb(120, 200, 200);
+  }
+
+  .button:hover {
+    cursor: pointer;
+  }
+
+  span:hover {
+    cursor: pointer;
+    text-decoration: underline;
   }
 `
 
 
-const Post = ({ post, handleLikePost, handleUnlikePost, handleDeletePost, userLiked }) => {
+const Post = ({ post, postAuthor, handleLikePost, handleUnlikePost, handleDeletePost, userLiked }) => {
   const [isLiked, setIsLiked] = useState(userLiked)
-  const postAuthor = post.user[0]
+  const navigate = useNavigate()
 
-  const handleLikeButton = () => {
+  const handleLikeButton = (e) => {
+    e.stopPropagation()
     if (isLiked) {
       handleUnlikePost()
     } else {
@@ -26,18 +44,32 @@ const Post = ({ post, handleLikePost, handleUnlikePost, handleDeletePost, userLi
     setIsLiked(!isLiked)
   }
 
+  const handleDeleteButton = (e) => {
+    e.stopPropagation()
+    handleDeletePost()
+  }
+
+  const viewUser = (e) => {
+    e.stopPropagation()
+    navigate(`/main/${postAuthor.username}`)
+  }
+
+  const viewPost = () => {
+    navigate(`/main/${postAuthor.username}/${post.id}`)
+  }
+
   return (
-    <StyledItem liked={isLiked}>
+    <StyledItem liked={isLiked} onClick={viewPost}>
       <div>
         content: {post.content}
       </div>
       <div>
         likes: {post.likes}
       </div>
-      <Button className="postLikeButton" text="like" handleClick={handleLikeButton} />
-      <Button className="postDeleteButton" text="delete" handleClick={handleDeletePost} />
+      <Button className="button postLikeButton" text="like" handleClick={handleLikeButton} />
+      <Button className="button postDeleteButton" text="delete" handleClick={handleDeleteButton} />
       <div>
-        by: <Link to={`${postAuthor.username}`}>{postAuthor.username}</Link>
+        by: <span onClick={viewUser}>{postAuthor.username}</span>
       </div>
     </StyledItem>
   )
