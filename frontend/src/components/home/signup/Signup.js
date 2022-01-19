91b70d3ef6276e4954ddd9494205dc22eb85cb58
Samuelisch <samuelchanwk@hotmail.com>
2022-01-19 19:46:40 +0800

    added Signup component and form component for signup

---
+++ frontend/src/components/home/signup/Signup.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import SignupForm from './SignupForm'
+import Container from '../Container'
+
+const Signup = () => {
+  return (
+    <Container>
+      <SignupForm />
+    </Container>
+  )
+}
+
+export default Signup
---
+++ frontend/src/components/home/signup/SignupForm.js
@@ -0,0 +1,126 @@
+import React from 'react'
+import Input from '../../assets/Input'
+import Button from '../../assets/Button'
+import { useDispatch } from 'react-redux'
+import { createUser } from '../../../reducers/userReducer'
+import { useNavigate } from 'react-router-dom'
+import styled from 'styled-components'
+
+const StyledContainer = styled.div`
+  width: 200px;
+`
+
+const StyledHeader = styled.h2`
+  font-size: 2rem;
+  color: rgb(245, 245, 245);
+`
+
+const StyledForm = styled.form`
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  margin: 40px 0 40px;
+  min-height: 120px;
+
+  .signupInput {
+    width: 95%;
+    background-color: rgb(15, 15, 15);
+    color: rgb(245, 245, 245);
+    margin-bottom: 10px;
+    border-radius: 4px;
+    border: 1px solid rgb(110, 110, 110);
+    padding: 8px 3px;
+  }
+
+  .signupButton {
+    margin-top: 20px;
+    padding: 5px;
+    border-radius: 15px;
+    background-color: rgb(240, 240, 240);
+    color: rgb(10, 10, 10);
+    font-weight: bold;
+  }
+
+  .signupButton:hover {
+    background-color: rgb(220, 220, 220);
+    cursor: pointer;
+  }
+`
+
+const SignupForm = () => {
+  const navigate = useNavigate()
+  const dispatch = useDispatch()
+
+  const signupHandler = async (e) => {
+    e.preventDefault()
+    const newName = e.target.signupName.value
+    const newUsername = e.target.signupUsername.value
+    const newPassword = e.target.signupPassword.value
+
+    const newUser = {
+      name: newName,
+      username: newUsername,
+      password: newPassword
+    }
+
+    try {
+      await dispatch(createUser(newUser))
+      e.target.reset()
+      console.log('success')
+      navigate('/login')
+    } catch (error) {
+      console.log(error)
+    }
+
+    e.target.signupPassword.value = ''
+  }
+
+  return (
+    <StyledContainer>
+      <StyledHeader>Create your account</StyledHeader>
+      <StyledForm onSubmit={signupHandler}>
+        <div>
+          <div>
+          <Input
+              className="signupInput"
+              name="signupName"
+              id="signupName"
+              type="text"
+              placeholder="Name"
+            />
+          </div>
+          <div>
+            <Input
+              className="signupInput"
+              name="signupUsername"
+              id="signupUsername"
+              type="text"
+              placeholder="New username"
+            />
+          </div>
+          <div>
+            <Input
+              className="signupInput"
+              name="signupPassword"
+              id="signupPassword"
+              type="password"
+              placeholder="New password"
+            />
+          </div>
+          {/*<div>
+            <Input
+              className="signupInput"
+              name="signupPassword2"
+              id="signupPassword2"
+              type="password"
+              placeholder="Confirm password"
+            />
+          </div>*/''}
+        </div>
+        <Button className="signupButton" type="submit" text="Sign up" />
+      </StyledForm>
+    </StyledContainer>
+  )
+}
+
+export default SignupForm
