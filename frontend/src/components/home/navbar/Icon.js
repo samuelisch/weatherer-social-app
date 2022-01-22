5f63ba1bd2068d5036493eb3ae25062d77f7a8e8
Samuelisch <samuelchanwk@hotmail.com>
2022-01-22 13:30:43 +0800

    added font awesome icons for navbar

--- frontend/src/components/home/navbar/Icon.js
+++ frontend/src/components/home/navbar/Icon.js
@@ -1,5 +1,7 @@
 import React from 'react'
 import styled from 'styled-components'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faSmog, faHome , faSearch, faUser, faCloudSunRain } from '@fortawesome/free-solid-svg-icons'
 
 const StyledIcon = styled.div`
   padding: 7px;
@@ -9,6 +11,9 @@ const StyledContainer = styled.div`
   width: 55px;
   height: 55px;
   border-radius: 50%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
 
   &:hover {
     background: rgba(100, 100, 100, 0.2);
@@ -20,7 +25,20 @@ const Icon = ({iconType}) => {
   return (
     <StyledIcon>
         <StyledContainer>
-          <span>icon</span>
+          <FontAwesomeIcon 
+            icon={
+              iconType === 'search'
+              ? faSearch
+              : iconType === 'profile'
+              ? faUser
+              : iconType === 'weather'
+              ? faCloudSunRain
+              : iconType === 'home'
+              ? faHome
+              : faSmog
+            } 
+            size="2x"
+          />
         </StyledContainer>
       </StyledIcon>
   )
