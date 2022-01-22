99a18eed3a25c78a66419af9098f025bb55142e0
Samuelisch <samuelchanwk@hotmail.com>
2022-01-22 20:38:33 +0800

    styled Post Form component, added container enclosing button, for better styling. added functionality for inputChange for textbox component, so that textbox's height will dynamically fit its content

--- frontend/src/components/home/posts/PostForm.js
+++ frontend/src/components/home/posts/PostForm.js
@@ -1,11 +1,67 @@
-import React from 'react'
-import Input from '../../assets/Input'
+import React, { useState } from 'react'
+import styled from 'styled-components'
+import Textbox from '../../assets/Textbox'
 import { useDispatch } from 'react-redux'
 import { createPost } from '../../../reducers/postReducer'
 import Button from '../../assets/Button'
 
+const StyledForm = styled.form`
+  display: flex;
+  flex-direction: column;
+`
+
+const StyledTextbox = styled(Textbox)`
+  margin: 20px 0 10px;
+  background: transparent;
+  border: none;
+  font-family: Helvetica;
+  font-size: 1.7rem;
+  resize: none;
+  height: 24px;
+  color: rgb(230, 230, 230);
+
+  &:focus {
+    outline: none;
+  }
+`
+
+const StyledContainer = styled.div`
+  padding: 0 10px;
+  display: flex;
+  justify-content: flex-end;
+`
+
+const StyledButton = styled(Button)`
+  padding: 8px 15px;
+  border: none;
+  border-radius: 20px;
+  background-color: rgba(90, 200, 90);
+  color: rgb(230, 230, 230);
+
+  &:hover {
+    background-color: rgba(90, 190, 90);
+  }
+
+  &:disabled {
+    opacity: 50%;
+
+    &:hover {
+      background-color: rgba(90, 200, 90);
+    }
+  }
+`
+
 const PostForm = () => {
   const dispatch = useDispatch()
+  const [textboxValue, setTextboxValue] = useState('')
+
+  //text input height change to fit content solution taken from link: https://stackoverflow.com/a/48460773
+  //manipulates height by adding scroll height
+  const handleInputChange = (e) => {
+    const textbox = e.target
+    textbox.style.height = 0;
+    textbox.style.height = textbox.scrollHeight + "px"
+  }
 
   const submitHandler = (e) => {
     e.preventDefault()
@@ -15,18 +71,25 @@ const PostForm = () => {
   }
 
   return(
-    <form onSubmit={submitHandler}>
-      Add post 
-      <Input
+    <StyledForm onSubmit={submitHandler}>
+      <StyledTextbox
+        className="newPostInput"
         name="content"
         type="text"
-        placeholder="content"
+        value={textboxValue}
+        handleChange={(e) => setTextboxValue(e.target.value)}
+        placeholder="What's happening?"
+        handleInput={handleInputChange}
       />
-      <Button 
-        type="submit"
-        text="create"
-      />
-    </form>
+      <StyledContainer>
+        <StyledButton 
+          className="newPostButton"
+          type="submit"
+          text="Create"
+          disabled={!textboxValue}
+        />
+      </StyledContainer>
+    </StyledForm>
   )
 }
 
