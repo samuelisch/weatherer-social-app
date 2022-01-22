eab23d5495860a04c140c3cba50ad5d7abf52686
Samuelisch <samuelchanwk@hotmail.com>
2022-01-22 11:14:36 +0800

    imported Navbar component to main Home file, added styling for navbar to be placed on the left

--- frontend/src/components/home/Home.js
+++ frontend/src/components/home/Home.js
@@ -1,14 +1,24 @@
 import React, { useState, useEffect } from 'react'
 import { useSelector, useDispatch } from 'react-redux'
-import Button from '../assets/Button'
-import { initializeLogin, logoutUser } from '../../reducers/loginReducer'
-import { useNavigate, Outlet } from 'react-router-dom'
+import { initializeLogin } from '../../reducers/loginReducer'
+import { Outlet } from 'react-router-dom'
 import { initializeUsers } from '../../reducers/userReducer'
 import { initializePosts } from '../../reducers/postReducer'
+import Navbar from './navbar/Navbar'
+import styled from 'styled-components'
+
+const StyledHome = styled.div`
+  max-width: 1440px;
+  margin: 0 auto;
+  display: flex;
+`
+
+const StyledContent = styled.div`
+  flex: 6 1 0;
+`
 
 const Home = () => {
   const [isLoaded, setIsLoaded] = useState(false)
-  const navigate = useNavigate()
   const dispatch = useDispatch()
   const users = useSelector(state => state.users)
   const user = useSelector(state => state.login)
@@ -27,21 +37,15 @@ const Home = () => {
     dispatch(initializePosts())
   }, [dispatch])
 
-  const handleLogout = async () => {
-    navigate('/main')
-    await dispatch(logoutUser())
-  }
-
   return (
-      <div className="home">
-        {isLoaded &&
-          <div>
-            Welcome {user.username}
-            <Button type='button' text='logout' handleClick={handleLogout} />
-          <Outlet />
-          </div>
-        }
-      </div>
+      <StyledHome>
+        <Navbar />
+        <StyledContent>
+          {isLoaded &&
+            <Outlet />
+          }
+        </StyledContent>
+      </StyledHome>
   )
 }
 
