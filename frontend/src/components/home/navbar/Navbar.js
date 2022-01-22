888ac67179b54e1cb377af1bcf7fa954a23f2261
Samuelisch <samuelchanwk@hotmail.com>
2022-01-22 19:39:14 +0800

    added moved logout button to Navbar component

--- frontend/src/components/home/navbar/Navbar.js
+++ frontend/src/components/home/navbar/Navbar.js
@@ -2,6 +2,16 @@ import React from 'react'
 import styled from 'styled-components'
 import { useNavigate } from 'react-router-dom'
 import Icon from './Icon'
+import Button from '../../assets/Button'
+import { logoutUser } from '../../../reducers/loginReducer'
+import { useDispatch } from 'react-redux'
+
+const StyledContainer = styled.div`
+  padding-bottom: 20px;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+`
 
 const StyledNav = styled.div`
   flex: 1 1 0;
@@ -9,15 +19,26 @@ const StyledNav = styled.div`
   flex-direction: column;
 `
 
-const Navbar = () => {
+const Navbar = ({ user }) => {
+  const navigate = useNavigate()
+  const dispatch = useDispatch()
+
+  const handleLogout = async () => {
+    navigate('/main')
+    await dispatch(logoutUser())
+  }
+
   return (
-    <StyledNav>
-      <Icon iconType="logo" />
-      <Icon iconType="home" />
-      <Icon iconType="search" />
-      <Icon iconType="profile" />
-      <Icon iconType="weather" />
-    </StyledNav>
+    <StyledContainer>
+      <StyledNav>
+        <Icon iconType="logo" handleNavigate={() => navigate('home')} />
+        <Icon iconType="home" handleNavigate={() => navigate('home')} />
+        <Icon iconType="search" handleNavigate={() => navigate('home')} />
+        <Icon iconType="profile" handleNavigate={() => navigate(`user/${user.username}`)} />
+        <Icon iconType="weather" />
+      </StyledNav>
+      <Button type='button' text='logout' handleClick={handleLogout} />
+    </StyledContainer>
   )
 }
 
--- frontend/src/components/home/posts/Posts.js
+++ frontend/src/components/home/posts/Posts.js
@@ -2,11 +2,6 @@ import React from 'react'
 import PostForm from './PostForm'
 import PostsList from './PostsList'
 import styled from 'styled-components'
-import Button from '../../assets/Button'
-import { logoutUser } from '../../../reducers/loginReducer'
-import { useNavigate } from 'react-router-dom'
-import { useDispatch } from 'react-redux'
-
 
 const StyledContentTop = styled.div`
   padding: 10px;
@@ -19,19 +14,10 @@ const StyledContentTop = styled.div`
 `
 
 const Posts = () => {
-  const navigate = useNavigate()
-  const dispatch = useDispatch()
-
-  const handleLogout = async () => {
-    navigate('/main')
-    await dispatch(logoutUser())
-  }
-
   return (
     <>
       <StyledContentTop>
           <h2>Home</h2>
-          <Button type='button' text='logout' handleClick={handleLogout} />
           <PostForm />
         </StyledContentTop>
       <PostsList />
