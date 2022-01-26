4b0fc9c6617c4551c4934eaf0606941d5182587d
Samuelisch <samuelchanwk@hotmail.com>
2022-01-26 19:52:56 +0800

    added a replyForm component, to be used on singlepostpage, reply popups

---
+++ frontend/src/components/home/posts/ReplyForm.js
@@ -0,0 +1,91 @@
+import React, { useState } from 'react'
+import styled from 'styled-components'
+import Textbox from '../../assets/Textbox'
+import Button from '../../assets/Button'
+import { useDispatch } from 'react-redux'
+import { useNavigate } from 'react-router-dom'
+import { replyPost } from '../../../reducers/postReducer'
+
+const StyledForm = styled.form`
+  display: flex;
+  align-items: center;
+`
+
+const StyledTextbox = styled(Textbox)`
+  margin: 10px 0;
+  width: 100%;
+  background: transparent;
+  border: none;
+  font-family: Helvetica;
+  font-size: 1.7rem;
+  resize: none;
+  height: 24px;
+  color: rgb(230, 230, 230);
+
+  &:focus {
+    outline: none;
+  }
+`
+
+const StyledButton = styled(Button)`
+  padding: 8px 15px;
+  border: none;
+  border-radius: 20px;
+  background-color: rgba(90, 200, 90);
+  color: rgb(230, 230, 230);
+
+  &:hover {
+    background-color: rgba(90, 190, 90);
+  }
+
+  &:disabled {
+    opacity: 50%;
+
+    &:hover {
+      background-color: rgba(90, 200, 90);
+    }
+  }
+`
+
+const ReplyForm = ({ post, popup }) => {
+  const dispatch = useDispatch()
+  const navigate = useNavigate()
+  const [textboxValue, setTextboxValue] = useState('')
+
+  const handleInputChange = (e) => {
+    const textbox = e.target
+    textbox.style.height = 0;
+    textbox.style.height = textbox.scrollHeight + "px"
+  }
+
+  const submitHandler = (e) => {
+    e.preventDefault()
+    const content = textboxValue
+    setTextboxValue('')
+    dispatch(replyPost(content, post.id))
+    if (popup) {
+      navigate(`/home/${post.id}`)
+    }
+  }
+
+  return (
+    <StyledForm onSubmit={submitHandler}>
+      <StyledTextbox
+        className="newReplyInput"
+        name="replyContent"
+        value={textboxValue}
+        handleChange={(e) => setTextboxValue(e.target.value)}
+        placeholder="Reply to post"
+        handleInput={handleInputChange}
+      />
+      <StyledButton 
+        className="newReplyButton"
+        type="submit"
+        text="Reply"
+        disabled={!textboxValue}
+      />
+    </StyledForm>
+  )
+}
+
+export default ReplyForm
