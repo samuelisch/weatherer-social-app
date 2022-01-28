6b46a0bf2dac31a9edf4536ecc1596b9d33cf28f
Samuelisch <samuelchanwk@hotmail.com>
2022-01-28 19:54:28 +0800

    added timestamp component to singlepost component

--- frontend/src/components/home/posts/SinglePost.js
+++ frontend/src/components/home/posts/SinglePost.js
@@ -5,6 +5,7 @@ import { useNavigate } from 'react-router-dom'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faArrowLeft } from '@fortawesome/free-solid-svg-icons'
 import ReplyForm from './ReplyForm'
+import TimeStamp from '../../assets/TimeStamp'
 
 const StyledContainer = styled.div`
   padding: 10px;
@@ -46,13 +47,13 @@ const StyledNameRow = styled.div`
 
   .name {
     margin-right: 5px;
-    font-size: 1.4rem;
+    font-size: 1.6rem;
     font-weight: bold;
   }
 
   .username {
     color: rgb(150, 150, 150);
-    font-size: 1.3rem;
+    font-size: 1.4rem;
 
     &:hover {
       text-decoration: none;
@@ -69,8 +70,8 @@ const StyledNameRow = styled.div`
 `
 
 const StyledContent = styled.div`
-  padding: 5px 0 10px;
-  font-size: 1.5rem;
+  padding: 10px 0 15px;
+  font-size: 2rem;
 `
 
 const SinglePost = ({ post, user }) => {
@@ -95,6 +96,7 @@ const SinglePost = ({ post, user }) => {
       <StyledContent>
         {post.content}
       </StyledContent>
+      <TimeStamp timestamp={post.date} />
       <hr />
       <PostIcons post={post} user={user} />
       <hr />
