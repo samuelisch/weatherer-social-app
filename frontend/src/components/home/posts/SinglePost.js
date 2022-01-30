1d127cf5fe4a05fcc9020eb8892983aa370dcd5e
Samuelisch <samuelchanwk@hotmail.com>
2022-01-30 13:16:00 +0800

    imported ReplyTo component in singlepostpage

--- frontend/src/components/home/posts/SinglePost.js
+++ frontend/src/components/home/posts/SinglePost.js
@@ -2,43 +2,13 @@ import React from 'react'
 import styled from 'styled-components'
 import PostIcons from './PostIcons'
 import { useNavigate } from 'react-router-dom'
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faArrowLeft } from '@fortawesome/free-solid-svg-icons'
 import ReplyForm from './ReplyForm'
 import TimeStamp from '../../assets/TimeStamp'
 
 const StyledContainer = styled.div`
-  padding: 10px;
-  border: 1px solid rgb(85, 85, 85);
-  border-width: 0 1px 1px;
-
   hr {
     border: none;
     border-top: 1px solid rgb(85, 85, 85);
-  }
-
-  .info {
-    display: flex;
-    align-items: center;
-    margin-bottom: 15px;
-  }
-
-  .backIcon {
-    padding: 7px;
-    font-size: 1.5rem;
-    border-radius: 50%;
-    transition: background-color 0.2s;
-
-    &:hover {
-      cursor: pointer;
-      background-color: rgba(215, 215, 215, 0.2);
-    }
-  }
-
-  .description {
-    margin: 0 15px;
-    font-weight: bold;
-    font-size: 1.8rem;
   }
 `
 
@@ -83,10 +53,6 @@ const SinglePost = ({ post, user }) => {
 
   return (
     <StyledContainer>
-      <div className="info">
-        <FontAwesomeIcon className="backIcon" icon={faArrowLeft} onClick={() => navigate(-1)} />
-        <span className="description">Thread</span>
-      </div>
       <StyledNameRow>
         <div className="nameDetails" onClick={viewUser}>
             <div className="name">{post.user.name}</div>
--- frontend/src/components/home/posts/SinglePostPage.js
+++ frontend/src/components/home/posts/SinglePostPage.js
@@ -1,12 +1,48 @@
 import React, { useState, useEffect } from 'react'
-import { useParams } from 'react-router-dom'
+import styled from 'styled-components'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faArrowLeft } from '@fortawesome/free-solid-svg-icons'
+import { useParams, useNavigate } from 'react-router-dom'
 import { useSelector } from 'react-redux'
 import { getPostFromId } from '../../../reducers/postReducer'
 import SinglePost from './SinglePost'
 import PostsList from './PostsList'
+import ReplyTo from './ReplyTo'
+
+const StyledPostContainer = styled.div`
+  padding: 10px;
+    border: 1px solid rgb(85, 85, 85);
+    border-width: 0 1px 1px;
+`
+
+const StyledInfo = styled.div`
+  display: flex;
+  align-items: center;
+  margin-bottom: 15px;
+
+  .backIcon {
+    padding: 7px;
+    font-size: 1.5rem;
+    border-radius: 50%;
+    transition: background-color 0.2s;
+
+    &:hover {
+      cursor: pointer;
+      background-color: rgba(215, 215, 215, 0.2);
+    }
+  }
+
+  .description {
+    margin: 0 15px;
+    font-weight: bold;
+    font-size: 1.8rem;
+  }
+`
 
 const SinglePostPage = () => {
   const [isLoaded, setIsLoaded] = useState(false)
+  const [postParent, setPostParent] = useState(null)
+  const navigate = useNavigate()
   const params = useParams()
   const { postId } = params
   const post = useSelector(state => getPostFromId(state.posts, postId))
@@ -24,10 +60,19 @@ const SinglePostPage = () => {
     <div>
       {isLoaded && 
         <>
-          <SinglePost 
-            post={post} 
-            user={user}
-          />
+          <StyledPostContainer>
+            <StyledInfo>
+              <FontAwesomeIcon className="backIcon" icon={faArrowLeft} onClick={() => navigate(-1)} />
+              <span className="description">Thread</span>
+            </StyledInfo>
+            {post.replyToPost &&
+              <ReplyTo postId={post.replyToPost} />
+            }
+            <SinglePost 
+              post={post} 
+              user={user}
+            />
+          </StyledPostContainer>
           <PostsList filterKey={post.id} type="replies" />
         </>
       }
