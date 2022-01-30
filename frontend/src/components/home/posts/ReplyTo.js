4cabe57225c1bd985c237845d720f63f2960e2a2
Samuelisch <samuelchanwk@hotmail.com>
2022-01-30 13:15:07 +0800

    created a replyTo component, users are able to view posts being replied to

---
+++ frontend/src/components/home/posts/ReplyTo.js
@@ -0,0 +1,27 @@
+import React from 'react'
+import styled from 'styled-components'
+import { getPostFromId } from '../../../reducers/postReducer'
+import { useSelector } from 'react-redux'
+import Post from './Post'
+
+const StyledContainer = styled.div`
+  border-left: 1px solid rgb(85, 85, 85);
+`
+
+const StyledParent = styled.div`
+  padding: 10px 0 20px 20px;
+`
+
+const ReplyTo = ({ postId }) => {
+  const post = useSelector(({ posts }) => getPostFromId(posts, postId))
+
+  return (
+    <StyledContainer>
+      <StyledParent>
+        <Post post={post} />
+      </StyledParent>
+    </StyledContainer>
+  )
+}
+
+export default ReplyTo
